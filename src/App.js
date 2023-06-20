fb3626658628cd52a93fc2856e20c86e078dfc45
Asif Raza <90905808+asifraza03@users.noreply.github.com>
2023-06-20 19:41:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar title="New Nav" home="Dhoom Machcale"/>
+    </>
+  );
+}
+
+export default App;
